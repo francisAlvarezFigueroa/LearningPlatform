b504938c1bbc847d55fb7bfe759799b3fafc5a02
matiasmjcs <matiasmejiascisternas@gmail.com>
2026-04-12 22:28:08 -0400

    feat: add Role and TransactionStatus enums

---
+++ LearningPlatform/src/main/java/com/duoc/LearningPlatform/model/Role.java
@@ -0,0 +1,6 @@
+package com.duoc.LearningPlatform.model;
+
+public enum Role {
+    STUDENT,
+    TEACHER
+}
---
+++ LearningPlatform/src/main/java/com/duoc/LearningPlatform/model/TransactionStatus.java
@@ -0,0 +1,7 @@
+package com.duoc.LearningPlatform.model;
+
+public enum TransactionStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
