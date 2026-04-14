ab62a5b287197c421f9b2a96bb8ede0f5d9ec0b4
matiasmjcs <matiasmejiascisternas@gmail.com>
2026-04-13 21:32:57 -0400

    chore: add seed data and configure H2 console

---
+++ LearningPlatform/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO courses (nombre_curso, nombre_instructor, curso_activo) VALUES ('Programacion en Java', 'Juan Perez', true);
+INSERT INTO courses (nombre_curso, nombre_instructor, curso_activo) VALUES ('Base de Datos Relacionales', 'Maria Gonzalez', true);
+INSERT INTO courses (nombre_curso, nombre_instructor, curso_activo) VALUES ('Desarrollo Web con HTML y CSS', 'Pedro Ramirez', true);
+INSERT INTO courses (nombre_curso, nombre_instructor, curso_activo) VALUES ('Introduccion a los Sistemas Operativos', 'Ana Torres', true);
+INSERT INTO courses (nombre_curso, nombre_instructor, curso_activo) VALUES ('Redes y Comunicaciones', 'Luis Castillo', false);
+INSERT INTO courses (nombre_curso, nombre_instructor, curso_activo) VALUES ('Fundamentos de Programacion', 'Maria Gonzalez', true);
